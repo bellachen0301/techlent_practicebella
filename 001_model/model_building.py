e894acdb736cdec4e55cc13445a61a29b93001e2
bellachen0301 <bellachen0301@gmail.com>
2022-07-13 21:32:48 -0300

    add new files

---
+++ 001_model/model_building.py
@@ -0,0 +1,13 @@
+import pickle
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+
+df = pd.read_csv('haberman.csv')
+X = df.iloc[:,:-1]
+y = df.iloc[:,-1] -1 # 0:survived 5+ years 1:died within 5 years
+
+model = LogisticRegression()
+model.fit(X,y)
+
+with open('model.pkl','wb') as f:
+    pickle.dump(model,f)
